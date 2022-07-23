687484241d16a361e205a8b74353b8bc83b41abc
Richard <xqtwkk@gmail.com>
2022-07-23 17:44:50 +0300

    Attempt 1

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task2;
 
+
 public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.println(first);
+        } else if(first < second) {
+            System.out.println(second);
+        } else {
+            System.out.println(first);
+        }
     }
 }
